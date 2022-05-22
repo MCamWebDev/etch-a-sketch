288b9133bcab1b6526f7110f6e576c993dfb412b
Miles Campbell <campbellmr2@yahoo.com>
2022-05-21 23:26:15 -0400

    Emplement Basic Grid
        -Link javascript file to index.html
        -Create a 16x16 grid of div elements using javascript

---
+++ etch-a-sketch.js
@@ -0,0 +1,17 @@
+
+const grid = document.querySelector('.grid');
+let grid_size = 16;
+
+for(let i = 0; i < grid_size; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    row.classList.add(i+1);
+    grid.appendChild(row);
+
+    for(let j = 0; j < grid_size; j++) {
+        const col = document.createElement('div');
+        col.classList.add('col');
+        col.classList.add(j+1);
+        row.appendChild(col);
+    }
+}
