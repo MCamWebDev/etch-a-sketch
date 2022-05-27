68890813a29221d63fd9b31d7f094a4b854e8214
Miles Campbell <campbellmr2@yahoo.com>
2022-05-26 21:15:24 -0400

    Make "Color" and "Grid Size" Buttons Functional
        -Add set_grid(num) and draw() functions to Javascript.
            -The draw() function listens to the grid elements for a
            mouseover event and sets the color to the value returned by
            the "Color" input.
    
            -The set_grid(num) function contains an event listener that
            listens for any change in the value returned by the "Grid
            Size" input and resets the grid to a size of num X num. It
            also calls the draw() function to link to the newly
            created html elements.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,31 +1,55 @@
-//select the container with "grid" class and set
+//select the container with "grid" class and set the grid size
 const grid = document.querySelector('.grid');
 
 //set grid size
-let grid_size = 16;
+const size = document.querySelector('#grid_size');
+let grid_size =size.value;
 
-//create a number of divs with "row" class equal to the grid size and inside each row 
-//create an equal amount of divs with "col" class
-for(let i = 0; i < grid_size; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    row.classList.add(i+1);
-    grid.appendChild(row);
+//function to create a number of divs with "row" class equal to 
+//the grid size and inside each row create an equal amount of 
+//divs with "col" class
+function set_grid(num) {
 
-    for(let j = 0; j < grid_size; j++) {
-        const col = document.createElement('div');
-        col.classList.add('col');
-        col.classList.add(j+1);
-        row.appendChild(col);
-    }
-}
+    for(let i = 0; i < num; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.classList.add(i+1);
+        grid.appendChild(row);
 
-//attach event listener to .col divs to change color when moused over
-const sketch = document.querySelectorAll('.col');
+        for(let j = 0; j < num; j++) {
+            const col = document.createElement('div');
+            col.classList.add('col');
+            col.classList.add(j+1);
+            row.appendChild(col);
+        };
+    };
+};
 
-sketch.forEach((div) => {
+//function to link the "color" input to elements
+function draw() {
+    //attach event listener to .col divs to change color when 
+    //moused over
+    let sketch = document.querySelectorAll('.col');
+    sketch.forEach((div) => {
 
-    div.addEventListener('mousemove', function (e) {
-        e.target.style.background = 'black';
+        div.addEventListener('mousemove', function (e) {
+            let color = document.querySelector('#color');
+            e.target.style.background = color.value;
+        });
     });
+};
+
+//initialize grid size and attach "color" input
+set_grid(grid_size);
+draw();
+
+//listen for change in grid size and then reset the grid and 
+//attach "color" input to newly created html elements
+size.addEventListener('change', () => {
+    grid_size = size.value;
+    grid.innerHTML = '';
+
+    set_grid(grid_size);
+    draw();
 });
+
