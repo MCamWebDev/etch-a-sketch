935a05691ad24ac29330680e61703b17eec1a110
Miles Campbell <campbellmr2@yahoo.com>
2022-06-04 23:14:37 -0400

    Improve the Readability of Javascript

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,22 +1,14 @@
-//select the container with "grid" class and set the grid size
-const grid = document.querySelector('.grid');
+//inputs are the location of where to place the grid and
+//the size of the grid to be created.
+function make_grid(loc, size) {
 
-//set grid size
-const size = document.querySelector('#grid_size');
-let grid_size =size.value;
-
-//function to create a number of divs with "row" class equal to 
-//the grid size and inside each row create an equal amount of 
-//divs with "col" class
-function set_grid(num) {
-
-    for(let i = 0; i < num; i++) {
+    for(let i = 0; i < size; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
         row.classList.add(i+1);
-        grid.appendChild(row);
+        loc.appendChild(row);
 
-        for(let j = 0; j < num; j++) {
+        for(let j = 0; j < size; j++) {
             const col = document.createElement('div');
             col.classList.add('col');
             col.classList.add(j+1);
@@ -25,31 +17,38 @@ function set_grid(num) {
     };
 };
 
-//function to link the "color" input to elements
-function draw() {
-    //attach event listener to .col divs to change color when 
-    //moused over
-    let sketch = document.querySelectorAll('.col');
-    sketch.forEach((div) => {
+
+//function inputs are an array of elements and a color.
+//the function attaches event listeners each div in the array of elements
+//and fills the background with the color provided when moused over.
+function draw(element_array, color_selection) {
+    
+    element_array.forEach((div) => {
 
         div.addEventListener('mousemove', function (e) {
-            let color = document.querySelector('#color');
-            e.target.style.background = color.value;
+            color_value = color_selection.value;
+            e.target.style.background = color_value;
         });
     });
 };
 
-//initialize grid size and attach "color" input
-set_grid(grid_size);
-draw();
 
-//listen for change in grid size and then reset the grid and 
-//attach "color" input to newly created html elements
-size.addEventListener('change', () => {
-    grid_size = size.value;
+//initialize grid location and grid size
+const grid = document.querySelector('.grid');
+const grid_size = document.querySelector('#grid_size');
+
+//creat initial grid and read in color selection
+make_grid(grid, grid_size.value);
+let selection = document.querySelectorAll('.col');
+let color_input = document.querySelector('#color');
+draw(selection, color_input);
+
+//reinitialize grid when the size is changed
+grid_size.addEventListener('change', () => {
+    let size = grid_size.value;
     grid.innerHTML = '';
 
-    set_grid(grid_size);
-    draw();
+    make_grid(grid, size);
+    let selection = document.querySelectorAll('.col');
+    draw(selection, color_input);
 });
-
